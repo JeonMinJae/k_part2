d7ae1914d11e3e20b3268d3dabb11f6e392a3a83
minjae <wjsalswo3@naver.com>
2020-02-16 21:17:59 +0900

    part2문제풀이

--- part2_mission/src/main/java/com/example/part2_mission/MainActivity.java
+++ part2_mission/src/main/java/com/example/part2_mission/MainActivity.java
@@ -1,12 +1,33 @@
 package com.example.part2_mission;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnok;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnok=(Button)findViewById(R.id.ok);
+        btnok.setOnClickListener(this);
+    }
+
+    private void showToast(String message){
+        Toast t=Toast.makeText(this, message, Toast.LENGTH_SHORT);
+        t.show();
+    }
+
+    @Override
+    public void onClick(View v){
+        if (v==btnok){
+            showToast("ok button click~~~~");
+        }
     }
 }
