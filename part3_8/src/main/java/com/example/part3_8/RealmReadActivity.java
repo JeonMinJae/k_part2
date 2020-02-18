0796836a6c06b92c69ed5ea68ed517a83f4a3a01
minjae <wjsalswo3@naver.com>
2020-02-18 13:51:59 +0900

    part3 퍼미션,파일다루기

---
+++ part3_8/src/main/java/com/example/part3_8/RealmReadActivity.java
@@ -0,0 +1,28 @@
+package com.example.part3_8;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import io.realm.Realm;
+
+public class RealmReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_realm_read);
+
+        TextView titleView=(TextView)findViewById(R.id.realm_read_title);
+        TextView contentView= (TextView)findViewById(R.id.realm_read_content);
+
+        Intent intent = getIntent();
+        String title = intent.getStringExtra("title");
+
+        Realm mRealm=Realm.getDefaultInstance();
+        MemoVO vo = mRealm.where(MemoVO.class).equalTo("title",title).findFirst();
+        titleView.setText(vo.title);
+        contentView.setText(vo.content);
+    }
+}
